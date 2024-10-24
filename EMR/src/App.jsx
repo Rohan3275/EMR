59711bcd2fdcc0e3db43f8e1377c5b385aed2381
AlfajShaikh <alfajshaikh55@gmail.com>
2024-10-24 20:21:57 +0530

    new update

--- EMR/src/App.jsx
+++ EMR/src/App.jsx
@@ -1,9 +1,9 @@
 
 import './App.css'
+import { PatientList } from './components/Appointments/PatientList/patientLists'
 import { Footer } from './components/Footer/footer'
 import { Home } from './components/HomePage/home'
 import { MainNavbar } from './components/navbar'
->>>>>>> d4609d48eeead48f0c4b27383e3698a9bb7d374c
 
 function App() {
 
