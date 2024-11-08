057d16d796e873b8e8603d7627635234553e9093
AlfajShaikh <alfajshaikh55@gmail.com>
2024-11-08 12:52:41 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { useTable } from 'react-table';
 import { Footer } from './components/Footer/footer'
 import { Home } from './components/HomePage/home'
 import { MainNavbar } from './components/navbar'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Registration from '../src/components/Patient/PatientRegistration';
 import { PatientListd } from './components/Patient/PatientList';
 import { PatientList } from './components/Appointments/PatientList/patientLists';
@@ -18,11 +18,11 @@ function App() {
     <BrowserRouter>
       <MainNavbar />
       <Routes>
-        
-        <Route path='/' element={<Home />}/>
-        <Route path='/pl' element={<PatientListd/>}/>
-        <Route path='/pr' element={<Registration/>}/>
-        <Route path='/p_list' element={<PatientList/>}/>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/pl' element={<PatientListd />} />
+        <Route path='/pr' element={<Registration />} />
+        <Route path='/p_list' element={<PatientList />} />
         {/* <Route path='/patient-card' element={<PatientCard/>}/> */}
       </Routes>
       <Footer />
