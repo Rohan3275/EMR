5a5348db01e58189d97bb6ba2be8a612b7d9e779
AlfajShaikh <alfajshaikh55@gmail.com>
2024-11-23 19:11:03 +0530

    my update 19:10

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -314,7 +314,7 @@ export function MainNavbar() {
   }, []);
 
   return (
-    <Navbar className=" max-w-screen-3xl px-4 py-2 fixed top-0 z-20">
+    <Navbar className=" max-w-screen-3xl px-4 py-2 fixed top-0 z-20 rounded-none">
       <div className="flex items-center justify-between text-blue-gray-900">
         <NavLink to={'/home'}>
           <Typography
